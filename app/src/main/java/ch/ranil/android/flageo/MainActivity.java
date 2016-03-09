fdd755955c97e35ff3f1c9e5b4df5f22aabe1f53
Ranil Wijeyratne <me@ranil.ch>
2016-03-09 23:17:11 +0100

    Introduced fragments

--- app/src/main/java/ch/ranil/android/flageo/MainActivity.java
+++ app/src/main/java/ch/ranil/android/flageo/MainActivity.java
@@ -1,37 +1,23 @@
 package ch.ranil.android.flageo;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import ch.ranil.android.flageo.model.Flag;
-import ch.ranil.android.flageo.model.Quiz;
-import ch.ranil.android.flageo.model.QuizBuilder;
+import ch.ranil.android.flageo.fragment.Name2FlagQuizFragment;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "Main";
+public class MainActivity extends AppCompatActivity implements Name2FlagQuizFragment.QuizAnswerListener {
 
     private static final long TIMER = 10000; // 10s
     private static final long TIMER_INTERVAL = 10; // 10ms
-    private static final int NUMBER_OF_CHOICES = 4;
 
     @Bind(R.id.progressBar)
     ProgressBar progressBar;
-
-    @Bind(R.id.txv_flagAsked)
-    TextView flagAsked;
-
-    private ImageButton[] flagButtons = new ImageButton[NUMBER_OF_CHOICES];
-    private Quiz<Flag> quiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        initializeQuizView();
+        loadQuiz();
+        initializeCountdown();
+    }
 
+    private void loadQuiz() {
+        Name2FlagQuizFragment quizFragment = Name2FlagQuizFragment.newInstance(4);
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer, quizFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+    private void initializeCountdown() {
         progressBar.setMax((int) (TIMER / TIMER_INTERVAL));
         CountDownTimer timer = new CountDownTimer(TIMER, TIMER_INTERVAL) {
 
@@ -60,58 +59,13 @@ public class MainActivity extends AppCompatActivity {
         timer.start();
     }
 
-    /**
-     * Initialize the flag buttons by selecting random flags and assigning them to the buttons.
-     */
-    private void initializeQuizView() {
+    @Override
+    public void quizAnswered(boolean correct) {
+        if (correct)
+            Toast.makeText(this, "Correct 8)", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Wrong dummy :#", Toast.LENGTH_SHORT).show();
 
-
-        flagButtons[0] = (ImageButton) findViewById(R.id.btn_flag1);
-        flagButtons[1] = (ImageButton) findViewById(R.id.btn_flag2);
-        flagButtons[2] = (ImageButton) findViewById(R.id.btn_flag3);
-        flagButtons[3] = (ImageButton) findViewById(R.id.btn_flag4);
-
-        quiz = QuizBuilder.buildFlagQuiz(Flag.class, NUMBER_OF_CHOICES);
-        Log.d(TAG, quiz.toString());
-
-        flagAsked.setText(quiz.getAnswer().getTranslation());
-
-        for (int i = 0; i < NUMBER_OF_CHOICES; i++) {
-            Flag f = quiz.getOption(i);
-            flagButtons[i].setImageResource(f.getDrawable());
-            flagButtons[i].setOnClickListener(flagButtonClickListener);
-        }
-    }
-
-    private View.OnClickListener flagButtonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.btn_flag1:
-                    processAnswer(0);
-                    return;
-                case R.id.btn_flag2:
-                    processAnswer(1);
-                    return;
-                case R.id.btn_flag3:
-                    processAnswer(2);
-                    return;
-                case R.id.btn_flag4:
-                    processAnswer(3);
-                    return;
-                default:
-                    Toast.makeText(getBaseContext(), "Unexpected button pressed: " + v.getId(), Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
-
-    private boolean processAnswer(int answer) {
-        if (quiz.isCorrect(answer)) {
-            Toast.makeText(getBaseContext(), "Correct!", Toast.LENGTH_SHORT).show();
-            return true;
-        } else {
-            Toast.makeText(getBaseContext(), "Wrong you fool!!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
+        loadQuiz();
     }
 }
---
+++ app/src/main/java/ch/ranil/android/flageo/fragment/Name2FlagQuizFragment.java
@@ -0,0 +1,146 @@
+package ch.ranil.android.flageo.fragment;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import ch.ranil.android.flageo.R;
+import ch.ranil.android.flageo.model.Flag;
+import ch.ranil.android.flageo.model.Quiz;
+import ch.ranil.android.flageo.model.QuizBuilder;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link QuizAnswerListener} interface
+ * to handle interaction events.
+ */
+public class Name2FlagQuizFragment extends Fragment {
+
+    private static final String PARAM_NUMBER_OF_CHOICES = "numberOfChoices";
+
+    @Bind(R.id.txv_flagAsked)
+    TextView flagAsked;
+
+    private int numberOfChoices = 4;
+    private Quiz<Flag> quiz;
+
+    private QuizAnswerListener answerListener;
+
+    public static Name2FlagQuizFragment newInstance(int numberOfChoices) {
+        Name2FlagQuizFragment fragment = new Name2FlagQuizFragment();
+        Bundle args = new Bundle();
+        args.putInt(PARAM_NUMBER_OF_CHOICES, numberOfChoices);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            numberOfChoices = getArguments().getInt(PARAM_NUMBER_OF_CHOICES);
+        }
+
+        quiz = QuizBuilder.buildFlagQuiz(Flag.class, numberOfChoices);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View fragmentLayout = inflater.inflate(R.layout.fragment_name2flag_quiz, container, false);
+        ButterKnife.bind(this, fragmentLayout);
+
+        ImageButton[] flagButtons = new ImageButton[numberOfChoices];
+
+        // TODO add dynamically to layout
+        flagButtons[0] = (ImageButton) fragmentLayout.findViewById(R.id.btn_flag1);
+        flagButtons[1] = (ImageButton) fragmentLayout.findViewById(R.id.btn_flag2);
+        flagButtons[2] = (ImageButton) fragmentLayout.findViewById(R.id.btn_flag3);
+        flagButtons[3] = (ImageButton) fragmentLayout.findViewById(R.id.btn_flag4);
+
+        flagAsked.setText(quiz.getAnswer().getTranslation());
+
+        for (int i = 0; i < flagButtons.length; i++) {
+            Flag f = quiz.getOption(i);
+            flagButtons[i].setImageResource(f.getDrawable());
+            flagButtons[i].setOnClickListener(flagButtonClickListener);
+        }
+
+        return fragmentLayout;
+    }
+
+    /**
+     * Track the click events of the answer buttons.
+     */
+    private View.OnClickListener flagButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_flag1:
+                    processAnswer(0);
+                    return;
+                case R.id.btn_flag2:
+                    processAnswer(1);
+                    return;
+                case R.id.btn_flag3:
+                    processAnswer(2);
+                    return;
+                case R.id.btn_flag4:
+                    processAnswer(3);
+                    return;
+                default:
+                    Toast.makeText(getActivity(), "Unexpected button pressed: " + v.getId(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    /**
+     * Check if the selected answer is correct and send info back to activity.
+     *
+     * @param answer selected answer
+     */
+    private void processAnswer(int answer) {
+        answerListener.quizAnswered(quiz.isCorrect(answer));
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof QuizAnswerListener) {
+            answerListener = (QuizAnswerListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement QuizAnswerListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        answerListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface QuizAnswerListener {
+        void quizAnswered(boolean correct);
+    }
+}
